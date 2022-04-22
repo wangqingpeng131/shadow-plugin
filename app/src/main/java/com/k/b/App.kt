6ed90c5f1c4a8338a4bdfa83412dfeac2e0fc4db
Bruce.wang <bruce.wang@gpower.co>
2022-04-22 10:59:48 +0800

    first commit

---
+++ app/src/main/java/com/k/b/App.kt
@@ -0,0 +1,9 @@
+package com.k.b
+
+import android.app.Application
+
+
+class App : Application() {
+
+
+}
---
+++ app/src/main/java/com/k/b/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.k.b
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.collagelike.postmore.R
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    // Container of objects shared across the whole app
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
